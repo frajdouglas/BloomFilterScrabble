6d293bd5648c56fbb39ff3b255fef291492de3c9
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-10-06 16:02:19 +0000

    removes wildcards

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const App = () => {
       "X",
       "Y", "Y",
       "Z",
-      "*", "*"
+      // "*", "*"
     ]
     let currentTileBag = shuffleBag(unshuffledTileBag)
     let newPlayers = []
